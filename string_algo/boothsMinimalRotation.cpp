c762da5cdb90f3e10a40b104f802198fc1eba158
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2021-05-18 15:49:32 +0600

    added booths algorithm for minimal rotation

--- string_algo/boothsMinimalRotation.cpp
+++ string_algo/boothsMinimalRotation.cpp
@@ -1,1 +1,33 @@
-///implement later
+///tested on cses
+int minimalRotation(string s)
+{
+    s+=s;
+    int n=s.length();
+    vector<int> f(n,-1);
+    int k=0;
+    int i;
+    for(int j=1;j<n;j++)
+    {
+        char sj=s[j];
+        i=f[j-k-1];
+        while(i!=-1&&sj!=s[k+i+1])
+        {
+            if(sj<s[k+i+1])
+                k=j-i-1;
+            i=f[i];
+        }
+        if(sj!=s[k+i+1])
+        {
+            if(sj<s[k])
+            {
+                k=j;
+            }
+            f[j-k]=-1;
+        }
+        else
+        {
+            f[j-k]=i+1;
+        }
+    }
+    return k;
+}
