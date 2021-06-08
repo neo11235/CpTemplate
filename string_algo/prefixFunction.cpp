1e0f85caca8a5bf2a9967f4ed824d17daa4cf59f
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2021-06-08 17:32:46 +0600

    added a version of prefix function

--- string_algo/prefixFunction.cpp
+++ string_algo/prefixFunction.cpp
@@ -22,3 +22,21 @@ void pi_function(string const &str,vector<int> & pi)
     }
     return;
 }
+//computes pi(s1+'#'+s2)
+//tested only once
+void pi_function(string const& s1,string const& s2,vector<int> &pi)
+{
+    int l1=(int)s1.length();
+    int l2=(int)s2.length();
+    pi_function(s1,pi);
+    pi[l1]=0;
+    int l=(l1+l2+1);
+    for(int i=l1+1;i<l;i++)
+    {
+        int j=pi[i-1];
+        while(j>0&&s2[i-l1-1]!=s1[j])
+            j=pi[j-1];
+        if(s2[i-l1-1]==s1[j])j++;
+        pi[i]=j;
+    }
+}
