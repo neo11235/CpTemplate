b00fb9fe97ac16d56df5f11dff7a3ea9437048af
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2021-06-30 13:44:35 +0600

    added another version

--- string_algo/zFunction.cpp
+++ string_algo/zFunction.cpp
@@ -16,3 +16,20 @@ void z_function(char* str,int sz,int* z){
     return;
 }
 /*********************************************/
+void z_function(const string& str,vector<int>& z){
+    int sz=str.size();
+    z[0]=0;//really dont know why i assigned z[0]=1 in previous implementation
+    int i,l=0,r=0,j,k;
+    for(i=1;i<sz;i++){
+        z[i]=0;
+        if(i<=r){
+            z[i]=min(r-i+1,z[i-l]);
+        }
+        while(i+z[i]<sz&&str[z[i]]==str[i+z[i]])z[i]++;
+        if(i+z[i]-1>r){
+            l=i;
+            r=i+z[i]-1;
+        }
+    }
+    return;
+}
