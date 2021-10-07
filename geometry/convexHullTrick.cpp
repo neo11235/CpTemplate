a385360d24470c7cbb41164552284264e35c74f8
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2021-10-07 14:20:20 +0600

    Modified CHT for monotone query

--- geometry/convexHullTrick.cpp
+++ geometry/convexHullTrick.cpp
@@ -246,6 +246,18 @@ public:
         else
             insertFront(nl);
     }
+    ll fastQuery(ll x)//if monotone query satisfied //not tested,although it should be ok
+    {
+        #ifdef INCREASING_QUERY
+        while(Q.size()>1&&Q[0].intersect(Q[1]).first<x)
+            Q.pop_front();
+        return Q[0](x);
+        #else
+        while(Q.size()>1&&Q.back().intersect(Q[(int)Q.size()-2]).first>x)
+            Q.pop_back();
+        return Q.back()(x);
+        #endif
+    }
     ll query(ll x)
     {
         pii indx=bSearch(x);
@@ -259,3 +271,40 @@ public:
     }
 };
 /***********************************************/
+
+/***********set based implementation
+source:https://github.com/kth-competitive-programming/kactl/blob/main/content/data-structures/LineContainer.h
+
+***************************/
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const { return k < o.k; }
+	bool operator<(ll x) const { return p < x; }
+};
+
+struct LineContainer : multiset<Line, less<>> {
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	static const ll inf = LLONG_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) return x->p = inf, 0;
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		auto l = *lower_bound(x);
+		return l.k * x + l.m;
+	}
+};
+
+/*********************************************/
