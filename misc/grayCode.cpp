c0b738fa4ccc84794a10296d6f91ca6e489a135b
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2021-10-07 18:40:14 +0600

    Added Gray code

---
+++ misc/grayCode.cpp
@@ -0,0 +1,13 @@
+/************
+source:https://github.com/AnikSarker/ACM-Library/blob/master/AdHoc/Gray%20Code.cpp
+*********/
+ll gray_code(ll x){return x ^ (x >> 1);}
+ll inverse_gray_code(ll x){
+    ll h = 1, res = 0;
+    do{
+        if (x & 1) res ^= h;
+        x >>= 1, h = (h << 1) + 1;
+    } while (x);
+    return res;
+}
+/***********************************************************/
