396ba6bc91f20139d14c6f3faec472068f7704ae
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2022-01-09 19:48:59 +0600

    updated convention

--- Graph/floyedWarshal.cpp
+++ Graph/floyedWarshal.cpp
@@ -1,14 +1,15 @@
-ll G[500][500];
+ll g[500][500];
 void floyedWarshal(int n)
 {
-    //G[i][i] should be zero
-    for(int k=0;k<n;k++){
+    //g[i][i] should be zero
+    for(int k=0;k<n;k++)
+    {
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(G[i][k]<infl&&G[k][j]<infl)
-                    G[i][j]=min(G[i][j],G[i][k]+G[k][j]);
+                if(g[i][k]<LLONG_MAX&&g[k][j]<LLONG_MAX)
+                    g[i][j]=min(g[i][j],g[i][k]+g[k][j]);
             }
         }
     }
