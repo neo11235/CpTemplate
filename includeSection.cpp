e6084d678e70c369edeb72cc42864737506a398d
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2022-06-17 11:33:57 +0600

    Added include section

---
+++ includeSection.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpii vector<pii >
+#define Graph vector<vector<int> >
+#define WGraph vector<vector<pii>>
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+#define mp(a,b) make_pair(a,b)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define bound(i,j,n,m) ((i)<n&&(j)<m&&(i)>=0&&(j)>=0)
+#define all(container) container.begin(),container.end()
