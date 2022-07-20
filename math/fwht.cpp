2a761a6a8f75519393145c7f845f8fecc4efe346
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2022-07-20 10:54:09 +0600

    Implemented fwht

---
+++ math/fwht.cpp
@@ -0,0 +1,22 @@
+///first implementation
+vector<ll> FWHT(vector<ll> P, bool inverse) {
+    int n=P.size();
+    assert((n&(n-1))==0);
+    for (int len = 1; 2 * len <= n; len <<= 1) {
+        for (int i = 0; i < n; i += 2 * len) {
+            for (int j = 0; j < len; j++) {
+                ll u = P[i + j];
+                ll v = P[i + len + j];
+                P[i + j] = u + v;
+                P[i + len + j] = u - v;
+            }
+        }
+    }
+
+    if (inverse) {
+        for (int i = 0; i < n; i++)
+            P[i] = P[i] / n;
+    }
+
+    return P;
+}
