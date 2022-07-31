8314816ae3f5161cb03e52ea1c2f9bef700477e7
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2022-07-31 21:41:24 +0600

    Added subset convolution

--- math/or_and_convo.cpp
+++ math/or_and_convo.cpp
@@ -171,4 +171,38 @@ void zinv(vll &d,int m)
 }
 
 //subset sum convolution
+//not fully tested,got some error if used with mod
+#define MAX_SIZE 1<<20
+ll f[MAX_SIZE];
+ll g[MAX_SIZE];
+ll res[MAX_SIZE];
+ll fhat[20][MAX_SIZE];
+ll ghat[20][MAX_SIZE];
+ll h[20][MAX_SIZE];
+void subsetConvolution(int m)
+{
+    int n=1<<m;
+    memset(fhat,0,sizeof(fhat));
+    memset(ghat,0,sizeof(ghat));
+    memset(res,0,sizeof(res));
+    memset(h,0,sizeof(h));
+    for(int i=0;i<n;++i)
+    {
+        fhat[__builtin_popcount(i)][i]=f[i];
+        ghat[__builtin_popcount(i)][i]=g[i];
+    }
+    for (int i=0; i<=m; i++) {
+        zeta(fhat[i],m);
+        zeta(ghat[i],m);
+        for (int j=0; j<=i; j++){
+            for (int mask = 0; mask < n; mask++){
+                h[i][mask] += fhat[j][mask]*ghat[i-j][mask];
+            }
+        }
+        zinv(h[i],m);
+    }
+    for(int  i=0;i<n;++i)
+        res[i]=h[__builtin_popcount(i)][i];
+}
+
 
