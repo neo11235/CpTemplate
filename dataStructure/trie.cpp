4846790cd1770f57b386ce2a4d8b2ec273279bc9
neo11235 <1805045@ugrad.cse.buet.ac.bd>
2022-09-06 19:48:44 +0600

    Added oop trie

--- dataStructure/trie.cpp
+++ dataStructure/trie.cpp
@@ -36,3 +36,41 @@ void addNumber(int x)
     }
     trie[curVertex].leaf=true;
 }
+
+/********Basic Trie*************/
+//also write for adding integer to trie
+template<int MAX_SIZE,int AS=26>
+struct Trie
+{
+    struct Node
+    {
+        int nxt[AS];
+        bool leaf;
+        Node(){
+            leaf=false;
+            memset(nxt,-1,sizeof(int)*AS);
+        }
+    };
+    Node trie[MAX_SIZE];
+    int curSize=1;
+    bool addString(string const& s)
+    {
+        int cur=0;
+        bool res=false;
+        bool r2=true;
+        for(auto e:s)
+        {
+            e-='0';
+            if(trie[cur].nxt[e]==-1)
+            {
+                res=true;
+                trie[cur].nxt[e]=curSize++;
+            }
+            if(trie[cur].leaf)
+                r2=false;
+            cur=trie[cur].nxt[e];
+        }
+        trie[cur].leaf=true;
+        return res&&r2;
+    }
+};
